00336e15acad3683cc60b972fb60a7042bb1ff8a
EtnikaHalili <57642537+EtnikaHalili@users.noreply.github.com>
2021-06-07 14:53:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myfirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
+    Button Login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.Password);
+
+        Login = findViewById(R.id.btnLogin);
+
+    }
+}
